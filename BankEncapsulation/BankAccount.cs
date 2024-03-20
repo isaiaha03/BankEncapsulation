74c802dc124c84396d3f8074a08d480b99896727
isaiaha03 <isaiahalvarez2003@gmail.com>
2024-03-20 13:12:59 -0500

    Completed exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double deposit)
+        {
+            balance += deposit;
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine("How much money would you like to deposit?");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(deposit);
+
+            Console.WriteLine($"Your current balance is: ${account.GetBalance()}");
         }
     }
 }
